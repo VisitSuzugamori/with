d097fd969b0838df5a4b4ae6b6c0b7028f41ce36
ISHIDA Naoto <naoto@isnot.jp>
2021-07-14 17:08:12 +0900

    chore: create nuxt app

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    commonjs: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'prettier',
+    'eslint:recommended',
+    'plugin:json/recommended',
+    'plugin:node/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier', 'node', 'no-secrets', 'jest', 'json'],
+  rules: {
+    'prettier/prettier': 0,
+    'node/exports-style': ['error', 'module.exports'],
+    'node/no-deprecated-api': ['error'],
+    semi: ['error', 'always'],
+    'semi-spacing': [
+      'error',
+      {
+        after: true,
+        before: false,
+      },
+    ],
+    'semi-style': ['error', 'last'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    'node/no-unsupported-features/es-syntax': 0,
+    'node/no-unpublished-import': 0,
+    'node/no-missing-import': 0,
+  },
+};
