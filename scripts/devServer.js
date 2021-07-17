c2431a6e4c7e86276b1cc5e56c8ee8b25a03a545
ISHIDA Naoto <naoto@isnot.jp>
2021-07-17 23:43:22 +0900

    chore: add dev server

---
+++ scripts/devServer.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const https = require('https');
+const connect = require('connect');
+const logger = require('morgan');
+const serveStatic = require('serve-static');
+
+const options = {
+  cert: fs.readFileSync('./cert/server.crt'),
+  key: fs.readFileSync('./cert/server.key')
+};
+
+const app = connect()
+      .use(logger('dev'))
+      .use(serveStatic('./docs/'))
+      .use(serveStatic('./static/'));
+
+https.createServer(options, app).listen(3000);
